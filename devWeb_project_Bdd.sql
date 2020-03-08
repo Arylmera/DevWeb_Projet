ee110a59ad2e383beb8092e125cf6076cc3822b4
Louis Brochet <louisbrochet@outlook.com>
2020-03-08 14:18:54 +0100

    update database

--- devWeb_project_Bdd.sql
+++ devWeb_project_Bdd.sql
@@ -1,10 +1,10 @@
 create schema if not exists devWeb_Project_Bdd collate utf8_general_ci;
 
-create table Caracteristiques
+create table Categories
 (
-    idCaracteristique   int(3) auto_increment
+    idCategorie   int(3) auto_increment
         primary key,
-    nameCaracteristique varchar(45) not null
+    nameCategorie varchar(45) not null
 );
 
 create table Points
@@ -17,13 +17,13 @@ create table Points
     primary key (idPoint)
 );
 
-create table CaracteristiquesPoints
+create table CategoriesPoints
 (
     idPointCP           int(5) not null,
-    idCaracteristiqueCP int(5) not null,
-    primary key (idPointCP, idCaracteristiqueCP),
+    idCategorieCP int(5) not null,
+    primary key (idPointCP, idCategorieCP),
     constraint CPC
-        foreign key (idCaracteristiqueCP) references Caracteristiques (idCaracteristique)
+        foreign key (idCategorieCP) references Categories (idCategorie)
             on update cascade on delete cascade,
     constraint CPP
         foreign key (idPointCP) references Points (idPoint)
@@ -45,19 +45,19 @@ INSERT INTO devWeb_Project_Bdd.Points (idPoint, namePoint, descriptionPoint, lat
 INSERT INTO devWeb_Project_Bdd.Points (idPoint, namePoint, descriptionPoint, latitudePoint, longitudePoint) VALUES (4, 'BouleauLAuzelle', 'Un regroupement de bouleaux centenaires provenant d''une espèce Russe qui pousse uniquement dans les forêt froides de la Sibérie.', 15.000000, 22.000000);
 INSERT INTO devWeb_Project_Bdd.Points (idPoint, namePoint, descriptionPoint, latitudePoint, longitudePoint) VALUES (5, 'SerresUCL', 'C''est ici que nos étudiants en plantes apprennent à cultiver tous types de plantes.', 17.000000, 28.000000);
 
-INSERT INTO devWeb_Project_Bdd.Caracteristiques (idCaracteristique, nameCaracteristique) VALUES (1, 'faune');
-INSERT INTO devWeb_Project_Bdd.Caracteristiques (idCaracteristique, nameCaracteristique) VALUES (2, 'flore');
-INSERT INTO devWeb_Project_Bdd.Caracteristiques (idCaracteristique, nameCaracteristique) VALUES (3, 'autres');
-INSERT INTO devWeb_Project_Bdd.Caracteristiques (idCaracteristique, nameCaracteristique) VALUES (4, 'Oiseaux');
-INSERT INTO devWeb_Project_Bdd.Caracteristiques (idCaracteristique, nameCaracteristique) VALUES (5, 'UCL');
+INSERT INTO devWeb_Project_Bdd.Categories (idCategorie, nameCategorie) VALUES (1, 'faune');
+INSERT INTO devWeb_Project_Bdd.Categories (idCategorie, nameCategorie) VALUES (2, 'flore');
+INSERT INTO devWeb_Project_Bdd.Categories (idCategorie, nameCategorie) VALUES (3, 'autres');
+INSERT INTO devWeb_Project_Bdd.Categories (idCategorie, nameCategorie) VALUES (4, 'Oiseaux');
+INSERT INTO devWeb_Project_Bdd.Categories (idCategorie, nameCategorie) VALUES (5, 'UCL');
 
 INSERT INTO devWeb_Project_Bdd.Photos (idPhoto, fichierPhoto, idPointPhoto) VALUES (1, 'foret.jpg', 2);
 
-INSERT INTO devWeb_Project_Bdd.CaracteristiquesPoints (idPointCP, idCaracteristiqueCP) VALUES (1, 1);
-INSERT INTO devWeb_Project_Bdd.CaracteristiquesPoints (idPointCP, idCaracteristiqueCP) VALUES (2, 2);
-INSERT INTO devWeb_Project_Bdd.CaracteristiquesPoints (idPointCP, idCaracteristiqueCP) VALUES (4, 2);
-INSERT INTO devWeb_Project_Bdd.CaracteristiquesPoints (idPointCP, idCaracteristiqueCP) VALUES (5, 2);
-INSERT INTO devWeb_Project_Bdd.CaracteristiquesPoints (idPointCP, idCaracteristiqueCP) VALUES (3, 3);
-INSERT INTO devWeb_Project_Bdd.CaracteristiquesPoints (idPointCP, idCaracteristiqueCP) VALUES (1, 4);
-INSERT INTO devWeb_Project_Bdd.CaracteristiquesPoints (idPointCP, idCaracteristiqueCP) VALUES (5, 5);
+INSERT INTO devWeb_Project_Bdd.CategoriesPoints (idPointCP, idCategorieCP) VALUES (1, 1);
+INSERT INTO devWeb_Project_Bdd.CategoriesPoints (idPointCP, idCategorieCP) VALUES (2, 2);
+INSERT INTO devWeb_Project_Bdd.CategoriesPoints (idPointCP, idCategorieCP) VALUES (4, 2);
+INSERT INTO devWeb_Project_Bdd.CategoriesPoints (idPointCP, idCategorieCP) VALUES (5, 2);
+INSERT INTO devWeb_Project_Bdd.CategoriesPoints (idPointCP, idCategorieCP) VALUES (3, 3);
+INSERT INTO devWeb_Project_Bdd.CategoriesPoints (idPointCP, idCategorieCP) VALUES (1, 4);
+INSERT INTO devWeb_Project_Bdd.CategoriesPoints (idPointCP, idCategorieCP) VALUES (5, 5);
 
