0ba3918f7014726e4bcc9a61d1570908ea5aebca
Arylmera <guillaume@lemer.be>
2020-03-31 15:25:41 +0200

    test wikiLink

--- Prod/src/app/components/point-sheet/point-sheet.component.ts
+++ Prod/src/app/components/point-sheet/point-sheet.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_BOTTOM_SHEET_DATA} from "@angular/material/bottom-sheet";
 import {PointsService} from "../../services/points/points.service";
 import {MapComponent} from "../map/map.component";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-point-sheet',
@@ -15,12 +16,23 @@ export class PointSheetComponent implements OnInit {
   ready = false;
 
   constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: any,
-              private pointsService: PointsService) { }
+              private pointsService: PointsService,
+              private http: HttpClient) { }
   ngOnInit(): void {
     this.ready = false;
     this.pointsService.recupPointById(this.data).subscribe( pointData => {
       this.point = pointData[0];
       this.ready = true;
     })
+  }
+
+  goToWiki(name: any) {
+    let nameURI = encodeURIComponent(name.trim());
+    this.http.get('https://fr.wikipedia.org/w/api.php?action=opensearch&search='+nameURI+'&limit=1&format=json').subscribe( data => {
+      console.log(data[0]);
+      if(data[0][3]){
+        window.open(data[0][3], '_bank');
+      }
+    });
   }
 }
